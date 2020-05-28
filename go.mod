605109fbfa682d8eda1116356cdb45d40dc7b3c5
mateus <mateuscdiniz@gmail.comgit config --global user.email mateuscdiniz@gmail.com>
2020-05-28 16:37:24 -0300

    Simple CRUD Golang using Postgres

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/mateuscdiniz/simplecrud
+
+go 1.13
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/mux v1.7.4
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.5.2
+)
---
+++ router/router.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/mateuscdiniz/simplecrud/middleware"
+)
+
+// Router is exported and used in main.go
+func Router() *mux.Router {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/user/{id}", middleware.GetUser).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/user", middleware.GetAllUser).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/newuser", middleware.CreateUser).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/user/{id}", middleware.UpdateUser).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/deleteuser/{id}", middleware.DeleteUser).Methods("DELETE", "OPTIONS")
+
+	return router
+}
